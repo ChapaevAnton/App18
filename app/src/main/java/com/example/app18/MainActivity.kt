dd0b402170be10c9d95ad85e6f33e749abef8884
ChapaevAnton <virsus2009@gmail.com>
2021-05-12 16:17:00 +0700

    19.1

--- app/src/main/java/com/example/app18/MainActivity.kt
+++ app/src/main/java/com/example/app18/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.app18
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-        val imageView = findViewById<ImageView>(R.id.image_view)
-        Glide.with(this).load(R.drawable.ic_launcher_background).circleCrop().into(imageView)
+
+        val root = findViewById<ViewGroup>(R.id.root)
+        val view = layoutInflater.inflate(R.layout.item, root, false)
+        view.visibility = View.INVISIBLE
+        val view1 = layoutInflater.inflate(R.layout.item, root, false)
+        view1.isEnabled = false
+        val view2 = layoutInflater.inflate(R.layout.item, root, false)
+        view2.setBackgroundColor(resources.getColor(R.color.red, theme))
+        val view3 = layoutInflater.inflate(R.layout.item, root, false)
+        view3.setOnClickListener {}
+        root.addView(view)
+        root.addView(view1)
+        root.addView(view2)
+        root.addView(view3)
+
     }
 }
