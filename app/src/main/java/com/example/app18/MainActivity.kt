0ff2721a9136d497adc42add50c10f288d02fe1a
ChapaevAnton <virsus2009@gmail.com>
2021-05-14 14:41:47 +0700

    19.4

--- app/src/main/java/com/example/app18/MainActivity.kt
+++ app/src/main/java/com/example/app18/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.app18
 
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +13,25 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-        val root = findViewById<ViewGroup>(R.id.root)
-        val view = layoutInflater.inflate(R.layout.item, root, false)
-        view.visibility = View.INVISIBLE
-        val view1 = layoutInflater.inflate(R.layout.item, root, false)
-        view1.isEnabled = false
-        val view2 = layoutInflater.inflate(R.layout.item, root, false)
-        view2.setBackgroundColor(resources.getColor(R.color.red, theme))
-        val view3 = layoutInflater.inflate(R.layout.item, root, false)
-        view3.setOnClickListener {}
-        root.addView(view)
-        root.addView(view1)
-        root.addView(view2)
-        root.addView(view3)
+//        val root = findViewById<ViewGroup>(R.id.root)
+//        val view = layoutInflater.inflate(R.layout.item, root, false)
+//        view.visibility = View.INVISIBLE
+//        val view1 = layoutInflater.inflate(R.layout.item, root, false)
+//        view1.isEnabled = false
+//        val view2 = layoutInflater.inflate(R.layout.item, root, false)
+//        view2.setBackgroundColor(resources.getColor(R.color.red, theme))
+//        val view3 = layoutInflater.inflate(R.layout.item, root, false)
+//        view3.setOnClickListener {}
+//        root.addView(view)
+//        root.addView(view1)
+//        root.addView(view2)
+//        root.addView(view3)
+
+
+        val root = findViewById<LinearLayout>(R.id.root)
+        val view4 = layoutInflater.inflate(R.layout.frame_item, root, true)
+
+
 
     }
 }
