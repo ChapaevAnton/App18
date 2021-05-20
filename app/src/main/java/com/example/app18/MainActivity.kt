54f7dcb1bb9e86588c40061ecaf2bc428acf2251
ChapaevAnton <virsus2009@gmail.com>
2021-05-20 16:39:17 +0700

    20.2

--- app/src/main/java/com/example/app18/MainActivity.kt
+++ app/src/main/java/com/example/app18/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.app18
 
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +8,17 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.view_all)
+
+        //TODO app20
+//        val rootContent = findViewById<LinearLayout>(R.id.root_content) //корневой layout
+//        val textView = TextView(this)
+//        rootContent.addView(textView.apply {
+//            text = "hello world!!!"
+//            setTextColor(ContextCompat.getColor(context, R.color.black))
+//        })
+
+        //TODO app18-19
 
 //        val root = findViewById<ViewGroup>(R.id.root)
 //        val view = layoutInflater.inflate(R.layout.item, root, false)
@@ -28,17 +35,17 @@ class MainActivity : AppCompatActivity() {
 //        root.addView(view3)
 
 
-        val rootContent = findViewById<LinearLayout>(R.id.root_content) //корневой layout
-        var count = 1 //счетчик элемента
-        val list: List<View> = List(100) {//заполняю список нумерованными элементами
-            val item = layoutInflater.inflate(R.layout.frame_item, rootContent, false) //элемент
-            val iconText = item.findViewById<TextView>(R.id.icon_text) //текст на элементе
-            iconText.setText("$count")  //передаем значение счетчика
-            count++ //увеличиваем счетчик
-            item //возвращаем готовый элемент
-        }
-
-        list.forEach { rootContent.addView(it) } //добавляем элемент в корневой layout
+//        val rootContent = findViewById<LinearLayout>(R.id.root_content) //корневой layout
+//        var count = 1 //счетчик элемента
+//        val list: List<View> = List(100) {//заполняю список нумерованными элементами
+//            val item = layoutInflater.inflate(R.layout.frame_item, rootContent, false) //элемент
+//            val iconText = item.findViewById<TextView>(R.id.icon_text) //текст на элементе
+//            iconText.setText("$count")  //передаем значение счетчика
+//            count++ //увеличиваем счетчик
+//            item //возвращаем готовый элемент
+//        }
+//
+//        list.forEach { rootContent.addView(it) } //добавляем элемент в корневой layout
 
     }
 }
